04667112fb621ff0d3cb78385c5a80ee8e327a60
Aryav Taneja <aryavtaneja@outlook.com>
2022-05-20 12:14:26 -0700

    docs: add javadoc description for Virus.java

--- src/Virus.java
+++ src/Virus.java
@@ -1,14 +1,34 @@
+/**
+ * Defines class for a virus in the simulator
+ * @author Ethan Hu
+ * @author Aryav Taneja
+ */
+
 package src;
 
 import java.util.ArrayList;
 
 public class Virus {
-    private double infectability;
+
+    /**
+     * @param infectability The probability that a person who interacts with an infected person also gets infected. 0-1 double.
+     * @param mortality A modifier for how lethal the disease is, although final chance of death is also a factor of susceptible 
+     * age group and preexisting conditions. 0-1 double.
+     * @param susceptibilities A boolean array of which age groups (children, adults, seniors) are more susceptible to the disease than others. Multiple are possible (i.e. children and seniors)
+     * @param incubation The number of days which the virus incubates in an infected person. The person is able to spread the
+     * disease in this time, but isn't able to die during this time.
+     * @param resistance The number of days that the person "feels symptoms" and is able to die. Every day of this period, they
+     * have a chance of death based on mortality, age and preexisting conditions. If they survive these many days, they are 
+     * considered recovered and cannot be infected again.
+     */
+    
+    private double infectability; 
     private double mortality;
     private boolean[] susceptibilities;
     private int incubation;
     private int resistance;
 
+    @Override
     public Virus(double infectability, double mortality, boolean[] susceptibilities, int incubation, int resistance) {
         this.infectability = infectability;
         this.mortality = mortality;
